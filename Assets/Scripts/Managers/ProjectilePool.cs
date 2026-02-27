a0a2f519fd0d9d3b055ddb50f16760d704803746
lilnoggi <itsamanihd@gmail.com>
2026-02-27 11:06:10 +0000

    Hairball Guns & Hairball Bullets
    Cats now have their own hairball guns that shoot hairballs

--- Assets/Scripts/Managers/ProjectilePool.cs
+++ Assets/Scripts/Managers/ProjectilePool.cs
@@ -9,6 +9,9 @@ public class ProjectilePool : MonoBehaviour
 {
     public static ProjectilePool Instance;  // Singleton reference
 
+    // A dictionary allows for multiple pools
+    private Dictionary<string, Queue<GameObject>> _poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
     [SerializeField] private GameObject _projectilePrefab;
     [SerializeField] private int _poolSize = 30;  // How many bullets to spawn at the start
 
@@ -31,25 +34,47 @@ public class ProjectilePool : MonoBehaviour
     }
 
     // WeaponSystem will call this to grab a bullet
-    public GameObject GetProjectile(Vector3 position, Quaternion rotation)
+    public GameObject GetProjectile(GameObject prefab, Vector3 position, Quaternion rotation)
     {
-        if (_pool.Count > 0)
+        string key = prefab.name;
+
+        // If this is the first time asking for this bullet, create a pool
+        if (!_poolDictionary.ContainsKey(key))
         {
-            GameObject obj = _pool.Dequeue();  // Take it out the queue
-            obj.transform.position = position;
-            obj.transform.rotation = rotation;
-            obj.SetActive(true);               // Turn it on
-            return obj;
+            _poolDictionary[key] = new Queue<GameObject>();
         }
 
-        // FAILSAFE: If run out of bullets, make a new one
-        return Instantiate(_projectilePrefab, position, rotation);
+        GameObject obj;
+
+        // Pull from the pool if there is a spare
+        if (_poolDictionary[key].Count > 0)
+        {
+            obj = _poolDictionary[key].Dequeue();
+        }
+        else
+        {
+            // Otherwise, make a new one and name it exactly like they key
+            obj = Instantiate(prefab);
+            obj.name = key;
+        }
+
+        obj.transform.position = position;
+        obj.transform.rotation = rotation;
+        obj.SetActive(true);  // Turn it on
+        return obj;
     }
 
     // The CheeseProjectile will call this to put itself back in the pool
     public void ReturnProjectile(GameObject obj)
     {
         obj.SetActive(false);  // Turn it off
-        _pool.Enqueue(obj);    // Put it back in line
+
+        // SAFETY CHECK: Ensure the dictionary key exists before putting it back
+        if (!_poolDictionary.ContainsKey(obj.name))
+        {
+            _poolDictionary[obj.name] = new Queue<GameObject>();
+        }
+
+        _poolDictionary[obj.name].Enqueue(obj);    // Put it back in line
     }
 }
